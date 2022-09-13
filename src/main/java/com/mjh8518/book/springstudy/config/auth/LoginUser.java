fce02ed9f6000240dc292b6aae8a7fbf677cfeda
mjh8518 <mjh851819@gmail.com>
2022-09-13 16:26:29 +0900

    chapter 05 - end

---
+++ src/main/java/com/mjh8518/book/springstudy/config/auth/LoginUser.java
@@ -0,0 +1,11 @@
+package com.mjh8518.book.springstudy.config.auth;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginUser {
+}
---
+++ src/main/java/com/mjh8518/book/springstudy/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package com.mjh8518.book.springstudy.config.auth.dto;
+
+import com.mjh8518.book.springstudy.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
